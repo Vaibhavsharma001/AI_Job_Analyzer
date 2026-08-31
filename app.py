01ab1de5f442a768c71d9d6b6b51e0cd8783cb28
vaibhavsharma <vaibhav53091@gmail.com>
2026-08-31 23:06:22 +0530

    Part 2: Add PDF text extraction

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import PyPDF2  #allows Python to read PDF files.
 
 st.title("AI & Job Analyzer")
 st.write("Analyze your resume against a job description")
@@ -6,4 +7,11 @@ st.subheader("Upload Your Resume")
 
 resume = st.file_uploader("Upload Your Resume",
                  type = ["pdf"])
+
+if resume:
+    pdf_reader = PyPDF2.PdfReader(resume)
+    
+    st.write("Resume uploaded successfully!")
+    st.write("Number of pages:", len(pdf_reader.pages))
+
 
